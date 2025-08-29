ad8d8d75b2bc7cdc1e7ba4af50b4d78a0dec42b3
le trung <lequoctrunggg@gmail.com>
2025-08-29 21:59:43 +0700

    fix vite config for vercel

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,19 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-  const repoName = env.VITE_REPO_NAME || "";
-
-  return {
-    base: env.VITE_REPO_NAME ? `/${env.VITE_REPO_NAME}/` : "/",
-    plugins: [react(), tailwindcss()],
-    server: {
-      open: true,
-      host: false,
-      proxy: {
-        "/api": {
-          target: "http://localhost:5000",
-          changeOrigin: true,
-          secure: true,
-        },
+export default defineConfig({
+  base: "/",
+  plugins: [react(), tailwindcss()],
+  server: {
+    open: true,
+    host: false,
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+        secure: true,
       },
     },
-  };
+  },
 });
